2fbced3cd46f825de26dcf9ff5b0f9eefd9861c8
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-10-21 21:33:08 -0200

    Desenvolvimento do teste da rota GET/:id

--- test/livro.js
+++ test/livro.js
@@ -42,8 +42,85 @@ describe('/GET livro', function() {
             res.should.have.status(200);
             //E em seguida retornar em um array todos os livros cadastrados na base de dados:
             res.body.should.be.a('array');
-            res.body.length.should.be.eql(0);
+            //res.body.length.should.be.eql(0);
         done();
+        });
+    });
+});
+
+/** 
+ * Teste da rota: /POST
+ */
+describe('/POST livro', function() {
+    it('Não deve retornar o POST do livro criado, uma vez que não foi definido o campo: paginas', function(done) {
+       
+        //Aqui simulamos a criação de um livro, porém sem definir a página do livro:
+        var livro = {
+            titulo: "Javascript. O Guia Definitivo",
+            autor: "David Flanagan",
+            ano: 2012
+        }
+        chai.request(server)
+        .post('/livro')
+        .send(livro)
+        .end(function(error, res) {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('errors');
+            res.body.errors.should.have.property('paginas');
+            res.body.errors.paginas.should.have.property('kind').eql('required');
+            done();
+        });
+    });
+    
+    it('Deve Criar um livro', function(done) {
+        var livro = {
+            titulo: "Javascript. O Guia Definitivo",
+            autor: "David Flanagan",
+            paginas:1080,
+            ano: 2012
+        } 
+        chai.request(server)
+        .post('/livro')
+        .send(livro)
+        .end(function(error, res) {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message').eql('Livro adicionado com Sucesso!');
+            res.body.livro.should.have.property('titulo');
+            res.body.livro.should.have.property('autor');
+            res.body.livro.should.have.property('paginas');
+            res.body.livro.should.have.property('ano');
+        done();
+        }); 
+    });
+});
+
+
+/** 
+ * Teste da rota: /GET/:id
+ */
+describe('/GET/:id livro', function() {
+    it('Deve retornar um livro dado o id', function() {
+        var livro = new Livro( {
+            titulo: "Javascript. O Guia Definitivo",
+            autor: "David Flanagan",
+            paginas:1080,
+            ano: 2012
+        });
+        livro.save(function(error, livro) {
+            chai.request(server)
+            .get('/livro/' + livro.id)
+            .send(livro)
+            .end(function(error, res) {
+               res.should.be.a('object');
+               res.body.should.have.property('titulo'); 
+               res.body.should.have.property('autor');
+               res.body.should.have.property('paginas');
+               res.body.should.have.property('ano');
+               res.body.should.have.property('_id').eql(livro.id);              
+        done();
+            });
         });
     });
 });
