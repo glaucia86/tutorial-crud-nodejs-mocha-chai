fb5e4efc8f4474c697ab92f7de18f0dbdbf7e053
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-10-21 20:05:36 -0200

    Início do desenvolvimento dos testes de unidade em Node.Js com Mocha & Chai.

--- test/livro.js
+++ test/livro.js
@@ -1,6 +1,35 @@
-/**
- * Arquivo: livro.js
+ /*
+ * Arquivo: routes/livro.js
  * Author: Glaucia Lemos
- * Description: 
- * Data: 16/10/2016
+ * Description: Arquivo responsável por realizar o TDD com Mocha & Chai no lado do server da nossa app.
+ * Data: 21/10/2016
+ * 
+ */
+
+process.env.NODE_ENV = 'test';
+
+var mongoose = require('mongoose');
+var Livro = require('../app/models/livro');
+
+//Aqui estamos declarando as dependências necessárias para realizar os nossos testes!
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+//Aqui é o bloco principal que executará o nossos testes:
+describe('Livros', function() {
+    beforeEach(function(done) {
+
+        //Sempre depois de executar o nosso teste, iremos limpar a nossa base de dados:
+        Livro.remove({}, function(error) {
+            done();
+        });
+    });
+});
+
+/** 
+ * Teste da rota: /GET
  */
